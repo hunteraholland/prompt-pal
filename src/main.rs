acc5038caaac6c2c76734f54cc77d8714448eb9e
Hunter Holland <huntholland@gmail.com>
2024-12-13 13:17:22 -0600

    feat: Enhance main function to utilize CLI arguments for dynamic input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod cli;
+
+use clap::Parser;
+use cli::Cli;
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // You can now use the CLI args like this:
+    if let Some(name) = cli.name.as_deref() {
+        println!("Value for name: {}", name);
+    }
+
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    match cli.debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is definitely on"),
+        _ => println!("Don't be crazy"),
+    }
 }
